35f16b41cba49dbbebb6d2c03c7e7d3367a1244a
Fanceir <fx_official@outlook.com>
2024-11-04 14:36:33 +0800

    feat: 手写promise

---
+++ 手写promise/final.js
@@ -0,0 +1,128 @@
+class myPromise {
+  constructor(executor) {
+    this.state = "pending";
+    this.value = undefined;
+    this.reason = undefined;
+    this.onFulfilledCallbacks = []; // 保存成功回调
+    this.onRejectedCallbacks = []; // 保存失败回调
+    try {
+      executor(this.resolve.bind(this), this.reject.bind(this));
+    } catch (e) {
+      this.reject(e);
+    }
+  }
+
+  resolve(value) {
+    if (this.state === "pending") {
+      this.state = "fulfilled";
+      this.value = value;
+      this.onFulfilledCallbacks.forEach((callback) => callback());
+    }
+  }
+
+  reject(reason) {
+    if (this.state === "pending") {
+      this.state = "rejected";
+      this.reason = reason;
+      this.onRejectedCallbacks.forEach((callback) => callback());
+    }
+  }
+
+  then(onFulfilled, onRejected) {
+    onFulfilled =
+      typeof onFulfilled === "function" ? onFulfilled : (value) => value;
+    onRejected =
+      typeof onRejected === "function"
+        ? onRejected
+        : (reason) => {
+            throw reason;
+          };
+    
+    const promise2 = new myPromise((resolve, reject) => {
+      if (this.state === "fulfilled") {
+        setTimeout(() => {
+          try {
+            let x = onFulfilled(this.value);
+            resolvePromise(promise2, x, resolve, reject);
+          } catch (e) {
+            reject(e);
+          }
+        });
+      }
+
+      if (this.state === "rejected") {
+        setTimeout(() => {
+          try {
+            let x = onRejected(this.reason);
+            resolvePromise(promise2, x, resolve, reject);
+          } catch (e) {
+            reject(e);
+          }
+        });
+      }
+
+      if (this.state === "pending") {
+        this.onFulfilledCallbacks.push(() => {
+          setTimeout(() => {
+            try {
+              let x = onFulfilled(this.value);
+              resolvePromise(promise2, x, resolve, reject);
+            } catch (e) {
+              reject(e);
+            }
+          });
+        });
+        this.onRejectedCallbacks.push(() => {
+          setTimeout(() => {
+            try {
+              let x = onRejected(this.reason);
+              resolvePromise(promise2, x, resolve, reject);
+            } catch (e) {
+              reject(e);
+            }
+          });
+        });
+      }
+    });
+    
+    return promise2;
+  }
+}
+
+function resolvePromise(promise2, x, resolve, reject) {
+  if (promise2 === x) {
+    return reject(new TypeError("Chaining cycle detected for promise"));
+  }
+  
+  if (x && (typeof x === "object" || typeof x === "function")) {
+    let used;
+    try {
+      let then = x.then;
+      if (typeof then === "function") {
+        then.call(
+          x,
+          (y) => {
+            if (used) return;
+            used = true;
+            resolvePromise(promise2, y, resolve, reject);
+          },
+          (r) => {
+            if (used) return;
+            used = true;
+            reject(r);
+          }
+        );
+      } else {
+        if (used) return;
+        used = true;
+        resolve(x);
+      }
+    } catch (e) {
+      if (used) return;
+      used = true;
+      reject(e);
+    }
+  } else {
+    resolve(x);
+  }
+}

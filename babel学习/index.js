5e6d4d010cc8f7a666a513c4f7000ebf9f565341
Fanceir <fx_official@outlook.com>
2025-01-07 22:08:12 +0800

    babel插件学习

--- babel学习/index.js
+++ babel学习/index.js
@@ -1,9 +1,30 @@
-//记得设置package.json的type为module
-import Babel from "@babel/core";
-import presetEnv from "@babel/preset-env";
-import fs from "node:fs";
+import Babel, { types } from "@babel/core";
+import fs from "fs";
 const file = fs.readFileSync("./test.js", "utf8");
+//() => {} 会被转换为 function() {}
+
+const transformFunction = ({ types: t }) => {
+  return {
+    name: "transform-function",
+    visitor: {
+      //匹配箭头函数转成funcion
+      ArrowFunctionExpression(path) {
+        console.log(path.node);
+        const node = path.node;
+        //async
+        const arrowFuntion = t.functionExpression(
+          null, //匿名函数
+          node.params, //参数
+          t.blockStatement([t.returnStatement(node.body)]), //函数体
+          node.generator,
+          node.async
+        );
+        path.replaceWith(arrowFuntion);
+      },
+    },
+  };
+};
 const result = Babel.transform(file, {
-  presets: [presetEnv],
+  plugins: [transformFunction],
 });
 console.log(result.code);
